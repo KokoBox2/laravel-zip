b2a4a32112ce5bc5f447aca3f1536c381aa9d709
Koko <kovacs.mate2012@gmail.com>
2026-09-15 13:39:01 +0200

    OraVege

--- resources/views/counties/index.blade.php
+++ resources/views/counties/index.blade.php
@@ -5,7 +5,11 @@
   <h1>Vármegyék</h1>
 
   @foreach($counties as $county)
-      <p>{{ $county->name }}</p>
+{{$county->name}}<form action="{{ route('counties.destroy', $county->id) }}" method="POST">
+  @csrf
+  @method('DELETE')
+  <button type="submit">Törlés</button>
+</form>
   @endforeach
   <a href="{{ route('counties.create') }}">Új megye</a>
 
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ use App\Http\Controllers\CountyController;
 Route::get('/', [CountyController::class, 'index'])->name('counties.index');
 Route::get('/counties/create', [CountyController::class, 'create'])->name('counties.create');
 Route::post('/counties', [CountyController::class, 'store'])->name('counties.store');
+Route::get('/counties/{county}', [CountyController::class, 'show'])->name('counties.show');
+Route::get('/counties/{county}/edit', [CountyController::class, 'edit'])->name('counties.edit');
+Route::patch('/counties/{county}', [CountyController::class, 'update'])->name('counties.update');
+Route::delete('/counties/{county}', [CountyController::class, 'destroy'])->name('counties.destroy');
