dfb075bd768c83021fa06c774fcb3ddb46bdc12d
Koko <kovacs.mate2012@gmail.com>
2026-09-15 13:01:58 +0200

    Progress
    Getting up to date

--- app/Http/Controllers/CountyController.php
+++ app/Http/Controllers/CountyController.php
@@ -16,51 +16,54 @@ class CountyController extends Controller
         return view('counties.index', compact('counties'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+        public function create()
     {
-        //
+        return view('counties.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+        ]);
+
+        $county = County::create($validated);
+
+        return redirect()
+            ->route('counties.show', $county)
+            ->with('status', 'Megye létrehozva!');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function show(County $county)
     {
-        //
+        return view('counties.show', compact('county'));
+        // API: return response()->json($county);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
+    public function edit(County $county)
     {
-        //
+        return view('counties.edit', compact('county'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function update(Request $request, County $county)
     {
-        //
+        $validated = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+        ]);
+
+        $county->update($validated);
+
+        return redirect()
+            ->route('counties.show', $county)
+            ->with('status', 'Megye frissítve!');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    public function destroy(County $county)
     {
-        //
+        $county->delete();
+
+        return redirect()
+            ->route('counties.index')
+            ->with('status', 'Megye törölve!');
     }
 }
